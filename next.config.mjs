ef2e3517ce9dad8d90d1cf49cfafe7f6c0158677
ialyramx-creator <ialyramx@gmail.com>
2026-02-27 01:25:30 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,24 @@ const repo = "StarHealthy";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Necesario para GitHub Pages (genera /out estático)
   output: "export",
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
-  images: { unoptimized: true },
   trailingSlash: true,
+
+  // Lo que ya tenías antes
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  // Importante en GitHub Pages (sin optimización server-side de imágenes)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
